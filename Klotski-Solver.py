9805c2b43995d1e5b0253dd68c39759e51986a94
rahtol <lothar.becke@t-online.de>
2023-01-19 19:08:50 +0100

    Initial Commit - still buggy.

---
+++ Klotski-Solver.py
@@ -0,0 +1,31 @@
+import collections
+
+import boardstate
+
+
+def solve():
+    count = 0
+    l = collections.deque([boardstate.initial_boardstate])
+    marked_boardstates = set()
+    while l:
+        current_boardstate = l.popleft()
+        if current_boardstate.is_final():
+            len_solution = 0
+            p = current_boardstate
+            while p is not None:
+                len_solution = len_solution + 1
+                p = p.pred
+            print(f'Solution of length {len_solution} found.')
+        admissible_moves = current_boardstate.find_admissible_moves()
+        for nx_cells in admissible_moves:
+            nx_boardstate = boardstate.Boardstate(nx_cells)
+            if nx_boardstate not in marked_boardstates:
+                marked_boardstates.add(nx_boardstate)
+                nx_boardstate.pred = current_boardstate
+                l.append(nx_boardstate)
+            else:
+                count = count + 1
+    print(f'#configurations={len(marked_boardstates)},  count={count}')
+
+
+solve()
